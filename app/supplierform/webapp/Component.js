a7e92581731ded23a31dff20c694bc42c748849e
Mohsin/918 <mohsinquantum@gmail.com>
2023-10-03 11:56:43 +0530

    display form link is already submitted

--- app/supplierform/webapp/Component.js
+++ app/supplierform/webapp/Component.js
@@ -1,7 +1,3 @@
-/**
- * eslint-disable @sap/ui5-jsdocs/no-jsdoc
- */
-
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/Device",
@@ -19,68 +15,61 @@ function (UIComponent, Device, models, BusyIndicator, MessageBox, Filter, format
             manifest: "json"
         },
 
-        /**
-         * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-         * @public
-         * @override
-         */
-        init: function () {
-            // call the base component's init function
+        async init() {
             UIComponent.prototype.init.apply(this, arguments);
 
-            var that = this;
+            try {
+                const data = await this.getDataFromServer();
+                this.handleData(data);
+                this.getRouter().initialize();
+            } catch (error) {
+                BusyIndicator.hide();
+                this.getRouter().navTo("invalidUrl", { status: "" });
+                this.getRouter().initialize();
+            }
+        },
 
-            this.id = jQuery.sap.getUriParameters().get("id");
-            var requestModel = this.getModel("request");
-            
-            var hardcodedURL = "https://impautosuppdev.launchpad.cfapps.ap10.hana.ondemand.com/a1aa5e6e-4fe2-49a5-b95a-5cd7a2b05a51.onboarding.spfiorionboarding-0.0.1";
-            var sPath = hardcodedURL + "/v2/odata/v4/catalog/VenOnboard?$filter=VendorId eq " + this.id;
+        async getDataFromServer() {
+            return new Promise((resolve, reject) => {
+                const id = jQuery.sap.getUriParameters().get("id");
+                const hardcodedURL = "https://impautosuppdev.launchpad.cfapps.ap10.hana.ondemand.com/a1aa5e6e-4fe2-49a5-b95a-5cd7a2b05a51.onboarding.spfiorionboarding-0.0.1";
+                const sPath = hardcodedURL + `/v2/odata/v4/catalog/VenOnboard?$filter=VendorId eq ${id}`;
 
-            var ajaxCall = new Promise(function(resolve, reject) {
                 $.ajax({
                     type: "GET",
                     contentType: "application/json",
                     url: sPath,
                     dataType: "json",
-                    success: function (data) {
-                        resolve(data);
-                    },
-                    error: function (error) {
-                        reject(error);
-                    }
+                    success: resolve,
+                    error: reject
                 });
             });
+        },
 
-            ajaxCall.then(function(data) {
-                requestModel.setData(data.d.results[0]);
-                requestModel.refresh(true);
-                if (data.d.results[0].Status === "SBS") {
-                    that.getRouter().navTo("invalidUrl", { status: "submit" });
-                    return;
-                }
+        handleData(data) {
+            const requestModel = this.getModel("request");
+            requestModel.setData(data.d.results[0]);
+            requestModel.refresh(true);
 
-                var today = new Date();
-                if ((today > new Date(data.d.results[0].VenValidTo)) || (today.toDateString() === new Date(data.d.results[0].VenValidTo).toDateString())) {
-                    MessageBox.error("Link Expired");
-                    return;
-                }
-                
-                if (data.d.results[0].VendorId === that.id) {
-                    that.getRouter().navTo("RouteView1", { "id": that.id });
-                } else {
-                    that.getRouter().navTo("invalidUrl", { status: "" });
-                }
-                // enable routing
-                that.getRouter().initialize();
+            if (data.d.results[0].Status === "SBS") {
+                this.getRouter().navTo("invalidUrl", { status: "submit" });
+                return;
+            }
 
-            }).catch(function(error) {
-                // Error logic here
-                BusyIndicator.hide();
-                that.getRouter().navTo("invalidUrl", { status: "" });
+            const today = new Date();
+            const expirationDate = new Date(data.d.results[0].VenValidTo);
 
-                // enable routing
-                that.getRouter().initialize();
-            });
+            if (today >= expirationDate) {
+                MessageBox.error("Link Expired");
+                return;
+            }
+
+            const id = jQuery.sap.getUriParameters().get("id");
+            if (data.d.results[0].VendorId === id) {
+                this.getRouter().navTo("RouteView1", { "id": id });
+            } else {
+                this.getRouter().navTo("invalidUrl", { status: "" });
+            }
         }
     });
 });
