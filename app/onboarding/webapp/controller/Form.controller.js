e61e61398ab304a6ef38c9fea26f6309827ffe3d
Rajesh Sehgal <gmanish478@gmail.com>
2024-05-20 13:35:20 +0000

    code fix

--- app/onboarding/webapp/controller/Form.controller.js
+++ app/onboarding/webapp/controller/Form.controller.js
@@ -706,7 +706,7 @@ sap.ui.define([
             },
             onAddressCodChange: function (oEvent) {
                 var addcode = oEvent.getSource().getValue();
-                this.isunitaddressexists = this.suppData.find(item => item.AddressCode === addcode) ? true : false;
+                this.isunitaddressexists = this.suppData.find(item => item.SupplierCode === addcode) ? true : false;
                 if (!this.isunitaddressexists) {
                     oEvent.getSource().setValueState("Error");
                     MessageBox.error("Address Code already exists. Kindly enter new Address Code");
